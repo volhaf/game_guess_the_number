9a1847aab53aa11276c9d7ebb6518cf27eaaa16a
volhaf <Volha.f@outlook.com>
2025-01-08 17:03:59 +0100

    refactor

--- js/js.js
+++ js/js.js
@@ -1,50 +1,75 @@
 'use strict';
-let sectetNumber = Math.trunc(Math.random() * 50) +1;
+let sectetNumber = Math.trunc(Math.random() * 50) + 1;
 let score = 20;
 let highscore = 0;
 
+
+const displayNumberInput = function (value) {
+    document.querySelector('.number-input').value = value;
+}
 const displayGuessMessage = function (message) {
     document.querySelector('.guess-message').textContent = message;
 }
+const displayGuessMessageStyle = function (message) {
+    document.querySelector('.guess-message').style.color = message;
+}
+const displayScoreMessage = function (value) {
+    document.querySelector('.score').textContent = value;
+}
+const displayQuestion = function (num) {
+    document.querySelector('.question').textContent = num;
+}
+const displayBodyStyle = function (style) {
+    document.querySelector('body').style.backgroundColor = style;
+}
+const displayQuestionStyle = function (style) {
+    document.querySelector('.question').style.width = style;
+}
+const displayScoreStyle = function (value) {
+    document.querySelector('.highscore').textContent = value;
+}
 
 
-document.querySelector('.check').addEventListener('click', function() {
+document.querySelector('.check').addEventListener('click', function () {
     const guessingNumber = Number(document.querySelector('.number-input').value);
     console.log(guessingNumber);
 
-    if(!guessingNumber) {
+    if (!guessingNumber) {
         displayGuessMessage('Input some number!')
 
     } else if (guessingNumber === sectetNumber) {
         displayGuessMessage('Сorrectly !!! ')
-        document.querySelector('.question').textContent = sectetNumber;
-        document.querySelector('body').style.backgroundColor = '#fff';
-        document.querySelector('.question').style.width = '50rem'
+        displayQuestion(sectetNumber)
+        displayBodyStyle('#fff');
+        displayQuestionStyle('50rem');
         if (score > highscore) {
             highscore = score
-            document.querySelector('.highscore').textContent = highscore;
+            displayScoreStyle(highscore)
         }
 
     } else if (guessingNumber !== sectetNumber) {
-        if(score > 1) {
+        if (score > 1) {
             displayGuessMessage(guessingNumber > sectetNumber ? 'too big' : 'too small');
             score = score - 1;
-            document.querySelector('.score').textContent = score;
+            displayScoreMessage(score);
         } else {
-            displayGuessMessage('Game Over!')
-            document.querySelector('.score').textContent = 0;
+            displayGuessMessage('Game Over!');
+            displayGuessMessageStyle('#FF0000');
+            displayScoreMessage(0);
+
         }
     }
 });
+
 document.querySelector('.again').addEventListener('click',
-    function() {
-        sectetNumber = Math.trunc(Math.random() * 50) +1;
+    function () {
+        sectetNumber = Math.trunc(Math.random() * 50) + 1;
         score = 20;
-    document.querySelector('.question').textContent = '???';
-        document.querySelector('.question').style.width = '25rem';
-        // document.querySelector('.guess-message').textContent = 'Start guessing !';
+        displayQuestion('???');
+        displayQuestionStyle('25rem');
         displayGuessMessage('Start guessing !')
-        document.querySelector('.score').textContent = score;
-        document.querySelector('.number-input').value = null;
-        document.querySelector('body').style.backgroundColor = '#000';
-})
+        displayScoreMessage(score);
+        displayNumberInput(null);
+        displayGuessMessageStyle('#0bdd11');
+        displayBodyStyle('#000');
+    })
